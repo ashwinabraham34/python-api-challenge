ef343fbd2fc4f6527072d2f3df548c142664c6e4
ashwinabraham34 <ashwin8abraham@yahoo.com>
2020-03-12 23:44:36 -0400

    final commit v2.0
    remove api keys

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "9f0f3f525b411be3df95581b302bf5e4"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyA8CA9e_BxkcpTtVQ6WJ2txcvKzzKlSGXw"
+g_key = ""
